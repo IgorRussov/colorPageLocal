f69efa31c47f27a33edd46126f0cfeeae3580567
IgorRussov <gorrussovs@gmail.com>
2022-11-18 14:04:09 +0200

    start and end line particles

--- Assets/Scripts/EffectsControl.cs
+++ Assets/Scripts/EffectsControl.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class EffectsControl : MonoBehaviour
 {
     public RectTransform uiCanvasTransform;
+    [Header("Text quip properties")]
     public List<Color> textStrokeColors;
     public List<string> textQuips;
-
     public GameObject textQuipPrefab;
+    [Header("End line particles")]
+    public GameObject endLineParticlesPrefab;
+
+
+    private bool textQuipMustGoLeft;
+    private float perfectStrokeMargin;
 
     private Color RandomTextColor()
     {
@@ -22,10 +28,31 @@ public class EffectsControl : MonoBehaviour
 
     public void BindToEvents(GameControl gameControl)
     {
-        gameControl.StrokeShapeFinished += SpawnQuip;
+        gameControl.StrokeShapeFinished += ShapeFinished;
+        perfectStrokeMargin = gameControl.perfectStrokeMargin;
     }
 
-    public void SpawnQuip(Vector3 worldPos, float error)
+    public void ShapeFinished(Vector3 pos, float error)
+    {
+        if (error < perfectStrokeMargin)
+        {
+            SpawnQuip(pos);
+            SpawnEndLineParticles(pos);
+        }
+    }
+
+    public void SpawnEndLineParticles(Vector3 worldPos)
+    {
+        GameObject newParticles = GameObject.Instantiate(endLineParticlesPrefab, transform);
+        newParticles.transform.position = worldPos;
+
+        float newScale = newParticles.transform.localScale.x;
+        float orthSize = Camera.main.orthographicSize;
+        newScale *= orthSize / 5;
+        newParticles.transform.localScale = Vector3.one * newScale;
+    }
+
+    public void SpawnQuip(Vector3 worldPos)
     {
         string text = RandomTextQuip();
         Color color = RandomTextColor();
@@ -34,7 +61,7 @@ public class EffectsControl : MonoBehaviour
         quipParent.AddComponent<RectTransform>();
         quipParent.transform.SetParent(uiCanvasTransform);
         Vector2 ViewportPosition = Camera.main.WorldToViewportPoint(worldPos);
-        Debug.Log("World pos: " + worldPos + ", viewport pos: " + ViewportPosition);
+        //Debug.Log("World pos: " + worldPos + ", viewport pos: " + ViewportPosition);
         float x = ViewportPosition.x * Camera.main.pixelWidth;
         float y = ViewportPosition.y * Camera.main.pixelHeight;
 
@@ -42,7 +69,8 @@ public class EffectsControl : MonoBehaviour
 
         GameObject quip = GameObject.Instantiate(textQuipPrefab, quipParent.transform);
 
-        quip.GetComponent<TextQuip>().SetTextAndColor(text, color);
+        quip.GetComponent<TextQuip>().SetProperties(text, color, !textQuipMustGoLeft);
+        textQuipMustGoLeft = !textQuipMustGoLeft;
     }
 
     // Start is called before the first frame update
